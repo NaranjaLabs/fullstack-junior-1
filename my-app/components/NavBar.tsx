a377bbd6dab2fd76328cd835f2faa5a8ccd8ef37
AllexThiagoSR <allexthiagodev@gmail.com>
2024-04-11 15:37:46 -0300

    style: Finishes the header styling.

--- my-app/components/NavBar.tsx
+++ my-app/components/NavBar.tsx
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 
-function NavBar() {
+function NavBar({ className }: { className: string }) {
   return (
     <nav>
-      <ul className={ `text-lg h-5 font-normal flex justify-evenly leading-5` }>
-        <li className='w-fit'>Home</li>
-        <li className='w-fit'>Service</li>
-        <li className='w-fit'>Portifolio</li>
-        <li className='w-fit'>How we works</li>
+      <ul className={ `text-lg h-5 font-normal flex justify-between leading-5 nav-list ` + className }>
+        <li className='w-fit'><Link href={''}>Home</Link></li>
+        <li className='w-fit'><Link href={''}>Service</Link></li>
+        <li className='w-fit'><Link href={''}>Portifolio</Link></li>
+        <li className='w-fit'><Link href={''}>How we works</Link></li>
       </ul>
     </nav>
   )
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -8,12 +8,22 @@ export default function Home() {
   return (
     <>
       <Image
-        src="/elipse1.svg" alt="My Icon" width={100} height={100}
+        src="/elipse1.svg" alt="Ellipse 1" width={100} height={100}
         className="elipse1 absolute z-0"
       />
-      <header className="header flex items-center justify-evenly">
+      {/* <Image
+        src="/elipse2.svg" alt="Ellipse 2" width={100} height={100}
+        className="elipse2 absolute z-0"
+      /> */}
+      <header className="header w-full flex items-center justify-between px-40">
         <h1 className={ `${poppins.className} logo font-semibold` }>Luxe</h1>
-        <NavBar />
+        <NavBar className={ `${poppins.className}` } />
+        <Image
+          alt="Menu Icon"
+          src="/menuIcon.svg"
+          height={32}
+          width={32}
+        />
       </header>
       <main>
         
