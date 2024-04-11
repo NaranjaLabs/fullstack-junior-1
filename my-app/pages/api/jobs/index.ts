5fe1f0f3dcef9551eb3cc23d6dcc6c341348c4c5
AllexThiagoSR <allexthiagodev@gmail.com>
2024-04-10 23:38:51 -0300

    feat: Accepts query parameters in the /api/jobs route

--- my-app/pages/api/jobs/index.ts
+++ my-app/pages/api/jobs/index.ts
@@ -14,6 +14,16 @@ type Error = {
 
 type Data = Job[] | Error;
 
+const getJobs = async (req: NextApiRequest): Promise<Job[]> => {
+  const jobs = await readFile(process.cwd() + '/public/jobs.json', 'utf-8');
+  const { level } = req.query
+  const parsedJobs: Job[] = JSON.parse(jobs);
+  if (level) {
+    return parsedJobs.filter((job) => job.level === level);
+  }
+  return parsedJobs;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
@@ -24,8 +34,8 @@ export default async function handler(
       return res.status(401).json({ message: "You don't have access to this route." })
     }
 
-    const jobs = await readFile(process.cwd() + '/public/jobs.json', 'utf-8')
-    return res.status(200).json(JSON.parse(jobs));
+    const jobs = await getJobs(req);
+    return res.status(200).json(jobs);
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error.' })
   }
